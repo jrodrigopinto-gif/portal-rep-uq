d48e78b6e82d63233d1ce797730af7a3514d0860
jrodrigopinto-gif <jrodrigopinto@gmail.com>
2026-08-08 18:45:15 -0300

    Add files via upload

---
+++ gemini-code-1786225320535.js
@@ -0,0 +1,34 @@
+const PRODUCTS = [
+  {
+    "id": "P0001",
+    "linha": "OTC",
+    "categoria": "Antimicótico",
+    "nome": "Andriodermol",
+    "principio_ativo": "Ácido undecilênico + associações",
+    "beneficios": [
+      "Combate micoses de pele e unha",
+      "Uso adulto e pediátrico acima de 2 anos"
+    ],
+    "selos": [
+      "ESTÁ ENTRE OS MAIS VENDIDOS DO MERCADO²",
+      "MARCA CONSAGRADA"
+    ],
+    "apresentacoes": [
+      {
+        "codigo": "1000026",
+        "apresentacao": "ANDRIODERMOL SOLUÇÃO TÓPICA X 50ML",
+        "ean13": "7896006214007",
+        "registro_ms": "1.0497.1152.003-6",
+        "cx_de": "42",
+        "pf": 20.88,
+        "pmc": 28.01,
+        "bula_url": "https://consultas.anvisa.gov.br/#/bulario/q/?numeroRegistro=1049711520036",
+        "preco_1_6": 18.79,
+        "preco_7_12": 18.37,
+        "preco_13_24": 17.96
+      }
+    ],
+    "foto": "",
+    "pagina": 6
+  }
+];
